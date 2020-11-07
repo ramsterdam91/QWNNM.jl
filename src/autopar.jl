39b7948f2a21ff372cb289604574f590e6348702
GiggleLiu <cacate0129@gmail.com>
2020-11-07 18:51:45 -0500

    ...

--- src/autopar.jl
+++ src/autopar.jl
@@ -1,49 +1,49 @@
 #Generate Parameters of WNNM-algorithm
 #input the nSig  the noise Level
 
-mutable struct PAR{T<:Int64, F<:Float32}
+struct PAR{T<:Int64, F<:Float32}
     nSig        ::T                         #level of Gaussian noise
     SearchWin   ::T                         #the search range for Neighbour Patches
     delta       ::F                         #parameter of iterative method
-    c           ::F              
+    c           ::F
     Innerloop   ::T
 
     patsize     ::T                         #the size of Patches is: patsize * patsize
-    patnum      ::T                   
+    patnum      ::Base.RefValue{T}
     Iter        ::T                         #parameter of iterative method
     lamada      ::F
     step        ::T
 end
 
 function autopar(nSig)
-  
-    SearchWin =   30  
-    delta     =   Float32(0.1) 
+
+    SearchWin =   30
+    delta     =   Float32(0.1)
     c         =   Float32(3*√(2))
     Innerloop =   2
 
     if          nSig <= 20
-        patsize       =   6             
-        patnum        =   70            
-        Iter          =   8            
-        lamada        =   Float32(0.54) 
+        patsize       =   6
+        patnum        =   70
+        Iter          =   8
+        lamada        =   Float32(0.54)
     elseif      nSig <= 40
-        patsize       =   7             
-        patnum        =   90           
-        Iter          =   12            
-        lamada        =   Float32(0.56)  
+        patsize       =   7
+        patnum        =   90
+        Iter          =   12
+        lamada        =   Float32(0.56)
     elseif      nSig <= 60
-        patsize       =   8             
-        patnum        =   120          
-        Iter          =   14            
-        lamada        =   Float32(0.58)  
+        patsize       =   8
+        patnum        =   120
+        Iter          =   14
+        lamada        =   Float32(0.58)
     else
-        patsize       =   9             
-        patnum        =   140           
-        Iter          =   14          
-        lamada        =   Float32(0.58)    
+        patsize       =   9
+        patnum        =   140
+        Iter          =   14
+        lamada        =   Float32(0.58)
     end
     step              =   patsize ÷ 2 -1
 
-    PAR(nSig, SearchWin, delta, c, Innerloop, patsize, patnum , Iter, lamada, step)
+    PAR(nSig, SearchWin, delta, c, Innerloop, patsize, Ref(patnum) , Iter, lamada, step)
 end
--- src/patEstimation.jl
+++ src/patEstimation.jl
@@ -1,6 +1,6 @@
 #slowwwwwwwwwwwwwwwwwww
 #Curent iteration Patches `CurPat`, Noise level of Patches `Sigma_arr`
-function patEstimation(NL_mat::Array{Int32,2}, Self_arr::Array{Int32,1}, Sigma_arr::Array{Float32,3}, CurPat::Array{Float32,3}, Par::PAR)
+function patEstimation(NL_mat::Array{Int32,2}, Self_arr::Array{Int32,1}, Sigma_arr::Array{Float32,2}, CurPat::Array{Float32,3}, Par::PAR)
     Depth = size(CurPat)[3]
 
     EPat   = zeros(Float32, size(CurPat))
@@ -13,11 +13,11 @@ function patEstimation(NL_mat::Array{Int32,2}, Self_arr::Array{Int32,1}, Sigma_a
         Temp    =   CurPat[:, NL_mat[:, i], :]
         M_Temp  =   Float32.(mean(Temp, dims = 2))
         Temp    =   Temp .- M_Temp
-        E_Temp 	=   WNNM(Temp, Par.c, Sigma_arr[1, Self_arr[i], :], M_Temp)
+        E_Temp 	=   WNNM(Temp, Par.c, Sigma_arr[Self_arr[i], :], M_Temp)
 
         EPat[:,NL_mat[:, i],:]    += E_Temp
         W[:,NL_mat[:, i], :]      += ones(Par.patsize^2, size(NL_mat[:, i])[1], Depth)
     end
-    
+
     (EPat, W)
 end
